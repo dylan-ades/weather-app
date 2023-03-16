e7d1aef26545f7c0d717aec7e176f0e8d66b2f2c
dylanades <dylanad21@gmail.com>
2023-03-16 15:20:53 -0400

    Made some improvements

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const apiKey = "235bfae9a77f01372c79720bbdbd549a"
 
 const $city = $('#city');
 const $temp = $('#temp');
+const $country = $('#country');
 const $weather = $('#weather');
 const $feels = $('#feels');
 const $input = $('.text1');
@@ -32,12 +33,21 @@ function handleGetData(event) {
 
 const convert = function(k) {
     let f = (k - 273.15)*1.8 + 32;
-    return f.toFixed(2);
+    return `${f.toFixed(2)}\u00B0 F`;
+}
+
+const firstLetterUppercase = function(word) {
+  const firstLetter = word.charAt(0)
+  const firstLetterCap = firstLetter.toUpperCase()
+  const remainingLetters = word.slice(1)
+  const capitalizedWord = firstLetterCap + remainingLetters
+  return capitalizedWord;
 }
 
 function render() {
     $city.text(weatherData.name);
+    $country.text(weatherData.sys.country);
     $temp.text(convert(weatherData.main.temp));
     $feels.text(convert(weatherData.main.feels_like));
-    $weather.text(weatherData.weather.description);
+    $weather.text(firstLetterUppercase(weatherData.weather[0].description));
  }
